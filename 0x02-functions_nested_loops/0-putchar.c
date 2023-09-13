90d815675116065131e43302c7e26e0bdde9e3af
Gich-M <gichmuriuki21@gmail.com>
2023-09-12 22:02:41 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <main.h>
+
+/*
+ * main - main function here
+ *
+ * Description: This program prints "_putchar" and returns 0.
+ *
+ * Return: Always 0
+ */
+int main()
+{
+        printf("_putchar\n");
+        return (0);
+}
