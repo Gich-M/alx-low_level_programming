1697236f29a7f0fe7fe5468511a3966b3be42f7d
Gich-M <gichmuriuki21@gmail.com>
2023-09-14 11:30:54 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - main function
+ * @c:c - input parameter
+ * int mul(int a, int b) - function to multiply integers
+ *
+ * Return: Always return 0.
+ */
+
+int mul(int a, int b);
+{
+	mul = a * b;
+	return mul;
+}
