a338c4b782acf86cf3d7ecfbe218a77b8f37e23b
Gich-M <gichmuriuki21@gmail.com>
2023-09-14 11:20:44 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,11 +8,10 @@
  * Return: Always return 0.
  */
 
-int _isupper(int c);
+int _isupper(int c)
 {
-
-	if (c >= '65' && c >= '90')
+	if (c >= 65 && c <= 90)
 		return (1);
 	else
-		return 0;
+		return (0);
 }
