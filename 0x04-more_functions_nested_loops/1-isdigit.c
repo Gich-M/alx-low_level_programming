0b7217eca03552a64678f4e4f75275ff25faaab3
Gich-M <gichmuriuki21@gmail.com>
2023-09-14 11:24:36 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - main function
+ * @c:c - input parameter
+ * int _isupper(int c) - checks for number
+ *
+ * Return: Always return 0.
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 49 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
