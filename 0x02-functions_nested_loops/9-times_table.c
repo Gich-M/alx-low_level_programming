6cb344c6b832c6025a17e3537005d1a17ef50c3f
Gich-M <gichmuriuki21@gmail.com>
2023-09-21 04:21:48 -0700

    times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table
+ * Return: 0 success
+ */
+void times_table(void)
+{
+	int i, mult, ans;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0');
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			ans = i * mult;
+
+			if (ans <= 9)
+				_putchar(' ');
+			else
+				_putchar((ans / 10) + '0');
+
+			_putchar((ans % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
