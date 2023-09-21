129da83ea05e63ebd2fabd58c70d4c9156824a0a
Gich-M <gichmuriuki21@gmail.com>
2023-09-20 23:31:33 -0700

    print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function prints 10 times the alphabet
+ *
+ * Return - void
+ */
+void print_alphabet_x10(void);
+{
+	char i;
+
+	int j;
+
+	for (j = 0; j <= 10; j++)
+	{
+		for (i = 0; i <= 10; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+}
