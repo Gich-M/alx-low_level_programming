07c766338bbd8902269776d234b0d7a9bc397325
Gich-M <gichmuriuki21@gmail.com>
2023-09-20 23:50:41 -0700

    print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,9 +11,9 @@ void print_alphabet_x10(void);
 
 	int j;
 
-	for (j = 0; j <= 10; j++)
+	for (j = 1; j <= 10; j++)
 	{
-		for (i = 0; i <= 10; i++)
+		for (i = 'a'; i <= 'z'; i++)
 		{
 			_putchar(i);
 		}
