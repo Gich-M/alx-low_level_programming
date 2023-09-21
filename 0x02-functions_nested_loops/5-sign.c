f739c10b91e21592fdb5c2b5a36229e77b074a08
Gich-M <gichmuriuki21@gmail.com>
2023-09-21 01:18:06 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_sign - function prints sign of a number
+ * @n: parameter to be checked
+ * Return: 1 for (n > 0) 0 for (n == 0) and -1 for (n < 0)
+ * _putchar: prints corresponding return types as + , 0 or -
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+		_putchar(+);
+		return (1);
+	else if (n == 0)
+		_putchar(0);
+		return (0);
+	else if (n < 0)
+		_putchar(-);
+		return (-1);
+}
