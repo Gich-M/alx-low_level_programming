9a1061c328ec9fcf44ab807ff054785f00aa9868
Gich-M <gichmuriuki21@gmail.com>
2023-09-21 00:33:25 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * int - input type parameters
+ *
+ * _islower - function checks for lowercase characters
+ *
+ * Return: returns 1 if lowercase and 0 if otherwise
+ *
+ */
+int _islower(int)
+{
+	/*function to check for lowercase characters*/
+	int c;
+
+	if (islower(c))
+		return (1);
+	else
+		return (0);
+}
