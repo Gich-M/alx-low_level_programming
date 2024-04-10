43440192b9f9074a1387b7bd67204997c5681c82
gichmuriuki21@gmail.com <gichmuriuki21@gmail.com>
2024-04-10 18:07:39 +0300

    bin_search_algo

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -2,52 +2,44 @@
 #include <stdio.h>
 
 /**
- * print_array - Prints an array of integers
- * @array: Pointer to the first element of the array
- * @left: Starting index of the subarray
- * @right: Ending index of the subarray
- */
-void print_array(int *array, size_t left, size_t right)
-{
-    size_t i;
-
-    printf("Searching in array: ");
-    for (i = left; i <= right; i++)
-        printf("%d, ", array[i]);
-    printf("\n");
-}
-
-/**
  * binary_search - Searches for a value in a sorted array
+ * of integers using the Binary search algorithm
  * @array: Pointer to the first element of the array to search in
  * @size: Number of elements in array
  * @value: Value to search
- * Return: The index of the value if found, otherwise -1
- * If value is not present in array or if array is NULL, your function must return -1
- * Print the array being searched every time it changes. (e.g. at the beginning and when you search a subarray)
+ * Return: The index where the value is found, or -1 if not present
  */
 int binary_search(int *array, size_t size, int value)
 {
-    size_t left = 0, right = size - 1;
-    size_t mid;
+	size_t left = 0;
+	size_t right = size - 1;
+	size_t mid;
 
-    if (array == NULL)
-        return (-1);
+	if (array == NULL)
+		return (-1);
 
-    print_array(array, left, right);
+	while (left <= right)
+	{
+		size_t i;
 
-    while (left <= right)
-    {
-        mid = (left + right) / 2;
-        if (array[mid] == value)
-            return (mid);
-        else if (array[mid] < value)
-            left = mid + 1;
-        else
-            right = mid - 1;
+		mid = (left + right) / 2;
+		printf("Searching in array: ");
 
-        print_array(array, left, right);
-    }
+		for (i = left; i <= right; i++)
+		{
+			printf("%d", array[i]);
+			if (i != right)
+				printf(", ");
+		}
+		printf("\n");
 
-    return (-1);
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
+
+	return (-1);
 }
