b0b898e87f1a7ad286bdb7eec6e4fba662b16a78
Gich-M <gichmuriuki21@gmail.com>
2024-05-11 02:54:05 -0700

    ht_set fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,33 +10,44 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
+	unsigned long int index, i;
 	hash_node_t *node;
+	char *v_copy;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 	{
 		return (0);
 	}
 
+	v_copy = strdup(value);
+	if (v_copy == NULL)
+		return (0);
+
 	index = key_index((const unsigned char *)key, ht->size);
 
-	node = malloc(sizeof(hash_node_t));
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = v_copy;
+			return (1);
+		}
+	}
 
+	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 	{
+		free(v_copy);
 		return (0);
 	}
 	node->key = strdup(key);
-	node->value = strdup(value);
-
-	if (node->key == NULL || node->value == NULL)
+	if (node->key == NULL)
 	{
-		free(node->key);
-		free(node->value);
 		free(node);
 		return (0);
 	}
-
+	node->value = v_copy;
 	node->next = ht->array[index];
 	ht->array[index] = node;
 
