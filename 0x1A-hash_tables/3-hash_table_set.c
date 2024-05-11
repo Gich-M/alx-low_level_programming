1c8db57e5fdaeb91174b2b219bd5c43a47574870
Gich-M <gichmuriuki21@gmail.com>
2024-05-11 02:58:56 -0700

    ht_set fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,16 +15,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	char *v_copy;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-	{
 		return (0);
-	}
 
 	v_copy = strdup(value);
 	if (v_copy == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-
 	for (i = index; ht->array[i]; i++)
 	{
 		if (strcmp(ht->array[i]->key, key) == 0)
