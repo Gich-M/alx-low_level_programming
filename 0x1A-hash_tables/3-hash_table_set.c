5595fd77ef762276da3a2262ce78908ec6302cb8
Gich-M <gichmuriuki21@gmail.com>
2024-05-11 02:35:08 -0700

    ht_set fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *node = node;
+	hash_node_t *node;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 	{
